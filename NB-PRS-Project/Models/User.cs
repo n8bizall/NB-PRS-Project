dd244c40a67562acc24e881c0852863244dab0f1
Nate Bucher <n8bizall@yahoo.com>
2018-03-09 14:23:00 -0500

    initital commit to put into github

---
+++ NB-PRS-Project/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NB_PRS_Project.Models
+{
+    public class User
+    {
+        public int MyProperty { get; set; }
+    }
+}
